762fe44075e78e05afe41942c4fe0f2ac58be23f
Sheetal Jaiswal <code2go77@gmail.com>
2022-10-08 18:16:52 +0530

    Intial checkin for movie service

---
+++ movie-catalog-service/src/test/java/com/code2go/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code2go.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
