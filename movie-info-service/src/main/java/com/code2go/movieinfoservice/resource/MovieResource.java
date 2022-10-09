aa64cb54022c0b4a13e32fefeddaf61c4e2a4651
Sheetal Jaiswal <code2go77@gmail.com>
2022-10-09 18:31:44 +0530

    Update files with proper integration

---
+++ movie-info-service/src/main/java/com/code2go/movieinfoservice/resource/MovieResource.java
@@ -0,0 +1,16 @@
+package com.code2go.movieinfoservice.resource;
+
+import com.code2go.movieinfoservice.model.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+        return new Movie(movieId, "test name");
+    }
+}
