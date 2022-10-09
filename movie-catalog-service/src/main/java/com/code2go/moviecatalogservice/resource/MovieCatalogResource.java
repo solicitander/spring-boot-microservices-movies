f67b9f5dc53543a21ed82fb26062071ec50917cc
Sheetal Jaiswal <code2go77@gmail.com>
2022-10-09 18:32:38 +0530

    Update files with proper integration

---
+++ movie-catalog-service/src/main/java/com/code2go/moviecatalogservice/resource/MovieCatalogResource.java
@@ -0,0 +1,52 @@
+package com.code2go.moviecatalogservice.resource;
+
+import com.code2go.moviecatalogservice.model.CatalogItem;
+import com.code2go.moviecatalogservice.model.Movie;
+import com.code2go.moviecatalogservice.model.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder weBuilder;
+
+    @RequestMapping("/{userId}")
+    List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
+        UserRating userRating = restTemplate.getForObject(
+                "http://localhost:8083/ratings/users/" + userId, UserRating.class);
+
+        return userRating.getRatings().stream().map(rating -> {
+            // For each movie ID, call movie info service and get details
+            Movie movie = restTemplate.getForObject(
+                    "http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+
+            // Put them all together
+            return new CatalogItem(movie.getName(), "Desc", rating.getRating());
+
+            //Using Web Client builder - New reactive way instead of RestTemplate
+            /*Movie movie = weBuilder.build()
+                    .get()
+                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();*/
+
+        })
+                .collect(Collectors.toList());
+
+    }
+}
